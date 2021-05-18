4188ad6e1c3f22f433b7c19792445c645a9936ff
Kruser <34228@nelnet.net>
2021-05-17 23:51:17 -0500

    Part4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -39,5 +39,38 @@ namespace GradeBook.GradeBooks
 
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly"
+                    + " calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            int numberOfGrades = 0;
+
+            foreach (Student student in Students)
+            {
+                if (student.Grades.Count > 0)
+                {
+                    numberOfGrades += 1;
+                }
+            }
+
+            if (numberOfGrades < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly"
+                    + " calculate a student's overall grade.");
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
