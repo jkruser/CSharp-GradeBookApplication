301d938271a52a8e4c50544a316c2b337e183e27
Kruser <34228@nelnet.net>
2021-05-17 23:41:49 -0500

    Part3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,34 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+
+            Students.Sort((x, y) => x.AverageGrade.CompareTo(y.AverageGrade));
+
+            int numberOfStudentsPerGrade = Students.Count / 5;
+
+            if (averageGrade >= Students[Students.Count - numberOfStudentsPerGrade].AverageGrade)
+            {
+                return 'A';
+            } else if (averageGrade >= Students[Students.Count - numberOfStudentsPerGrade * 2].AverageGrade)
+            {
+                return 'B';
+            } else if (averageGrade >= Students[Students.Count - numberOfStudentsPerGrade * 3].AverageGrade)
+            {
+                return 'C';
+            } else if (averageGrade >= Students[Students.Count - numberOfStudentsPerGrade * 4].AverageGrade)
+            {
+                return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
